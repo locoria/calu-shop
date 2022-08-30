71880521f2d346dc604af3154519759ef01faf30
Lorena Coria <locoria87@gmail.com>
2022-08-30 10:03:50 -0300

    mejoras

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,5 @@
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 
-<Navbar bg="dark" variant="dark">
-  <Container>
-    <Navbar.Brand href="#home">Calu Shop </Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#inicio">Inicio</Nav.Link>
-      <Nav.Link href="#productos">Productos</Nav.Link>
-      <Nav.Link href="#New In">New In </Nav.Link>
-    </Nav>
-  </Container>
-</Navbar>
 
-export default App;
+
