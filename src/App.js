1c75f4d2038a0395aa1a0ddb2e741809d7b304f9
Lorena Coria <locoria87@gmail.com>
2022-09-01 09:55:44 -0300

    cartwidget

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CartWidget from './components/CartWidget';
+import React from 'react';
 
 
-function App  ()  {
+const App = () => {
   return ( 
     
+  <>
+  <Navbar/>
+  <CartWidget/>
   
-  <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Calu Shop </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#inicio">Inicio</Nav.Link>
-        <Nav.Link href="#productos">Productos</Nav.Link>
-        <Nav.Link href="#New In">New In </Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
-  
+  </>
    );
   }
 
